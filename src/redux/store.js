1b3ec8e4020ebf2616b64c04091b2a3a3c787434
SalarC123 <Tabinda@chaudrys-iMac.local>
2021-04-07 19:37:25 -0500

    init: redux directory for reducers and store

---
+++ src/redux/store.js
@@ -0,0 +1,18 @@
+// import createStore and combineReducers
+// import all of the reducers
+// combine all of the reducers
+// initialize store with combined reducers
+// export default store
+
+import { createStore, combineReducers } from 'redux';
+import ParticleReducer from './ParticleReducer'
+import PolygonReducer from './PolygonReducer'
+
+const allReducers = combineReducers({
+    ParticleReducer,
+    PolygonReducer,
+})
+
+let store = createStore(allReducers)
+
+export default store;
