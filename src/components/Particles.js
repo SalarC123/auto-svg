b65053731b9ba0232b45d0f941ef93685e810dae
SalarC123 <Tabinda@chaudrys-iMac.local>
2021-04-09 20:04:43 -0500

    refactor: move particle logic to its own commponent

---
+++ src/components/Particles.js
@@ -0,0 +1,65 @@
+import {useSelector, useDispatch} from 'react-redux'
+import Slider from '@material-ui/core/Slider'
+import TextField from '@material-ui/core/TextField'
+import ColorPicker from 'material-ui-color-picker'
+import AutorenewIcon from '@material-ui/icons/Autorenew'
+
+function Particles() {
+
+    const particles = useSelector(store => store.ParticleReducer)
+    const dispatch = useDispatch()
+
+    return (
+        <>
+            {/* goes in dashboard component */}
+            <div className="controls">
+                <Slider 
+                    defaultValue={0}
+                    min={1} 
+                    max={500} 
+                    value={particles.coords.length} 
+                    onChange={(e, value) => {
+                        dispatch({type:"PARTICLE-COUNT", payload:value })
+                    }} 
+                    aria-labelledby="continuous-slider"
+                />
+
+                <ColorPicker 
+                    style={{margin:'1rem',}}
+                    label = "Color"
+                    defaultValue={"#000000"}
+                    value={particles.color}
+                    onChange={(value) => {
+                        dispatch({type:"PARTICLE-COLOR", payload: value})
+                    }}
+                />
+
+                <TextField 
+                    style={{margin:'1rem',}}
+                    type="number"
+                    label = "Maximum Size"
+                    value = {particles.maxSize}
+                    onChange = {(e) => {
+                        dispatch({type:"PARTICLE-MAXSIZE", payload: e.target.value})
+                    }}
+                />
+
+                <AutorenewIcon
+                    style={{margin:'1rem',width:"3rem", height:"3rem"}}
+                    onClick={() => {
+                        dispatch({type: "PARTICLE-COUNT", payload: particles.coords.length})
+                    }}
+                />
+
+            </div>
+        
+            <svg viewBox="0 0 100 100">
+                {particles.coords.map((coords) => (
+                <circle fill={particles.color} cx={coords[0]} cy={coords[1]} r={Math.random() * particles.maxSize}/>
+                ))}
+            </svg>
+        </>
+    )
+}
+
+export default Particles;
